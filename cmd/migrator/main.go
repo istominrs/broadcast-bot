787f660daacb4640b3099a1d632be81fc6b0ee6f
Roman Istomin <115490701+istominrs@users.noreply.github.com>
2024-06-06 18:59:12 +0300

    Create main.go

---
+++ cmd/migrator/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	var dsn, migrationsPath string
+
+	flag.StringVar(&dsn, "dsn", "", "path to storage")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "path to migrations")
+	flag.Parse()
+
+	if dsn == "" {
+		panic("dsn is required")
+	}
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New("file://"+migrationsPath, dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			log.Println("no migrations to apply")
+			return
+		}
+
+		panic(err)
+	}
+
+	log.Println("migrations applied")
+}
